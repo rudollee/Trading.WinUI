f97b13d12b683d75154c109d71e0a461c8e8457c
rudollee <rudollee@gmail.com>
2025-11-23 19:12:52 +0900

    observableCollection extension added

---
+++ Trading.WinUI/Extensions/ObservableCollectionEx.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Trading.WinUI.Extensions;
+
+public class ObservableCollectionEx<T> : ObservableCollection<T>
+{
+	public void AddRange(IEnumerable<T> collection)
+	{
+		CheckReentrancy();
+
+		if (!collection.Any()) return;
+
+		var itemsList = (List<T>)Items;
+		itemsList.AddRange(collection);
+
+		OnCollectionChanged(new NotifyCollectionChangedEventArgs(
+			action: NotifyCollectionChangedAction.Add,
+			changedItem: itemsList.Last(),
+			index: itemsList.Count - 1));
+	}
+
+	public void InsertRange(int index, IEnumerable<T> collection)
+	{
+		CheckReentrancy();
+
+		if (!collection.Any()) return;
+
+		var itemsList = (List<T>)Items;
+		itemsList.InsertRange(index, collection);
+
+		OnCollectionChanged(new NotifyCollectionChangedEventArgs(
+			action: NotifyCollectionChangedAction.Add,
+			changedItem: itemsList.First(),
+			index: index));
+	}
+}
