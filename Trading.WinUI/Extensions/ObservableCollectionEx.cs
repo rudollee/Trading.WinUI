b70fe22f2af00f0c43f67987d6b729a9e186fea5
sangwook <rudollee@gmail.com>
2026-05-11 23:26:43 +0900

    ObservableCollectionEx: lset _suppressCount thread-safe

--- Trading.WinUI/Extensions/ObservableCollectionEx.cs
+++ Trading.WinUI/Extensions/ObservableCollectionEx.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Threading;
 
 namespace Trading.WinUI.Extensions;
 
@@ -9,11 +10,11 @@ public class ObservableCollectionEx<T> : ObservableCollection<T>
 {
 	private int _suppressCount;
 
-	public void PauseUpdate() => _suppressCount++;
+	public void PauseUpdate() => Interlocked.Increment(ref _suppressCount);
 
 	public void ResumeUpdate()
 	{
-		if (--_suppressCount == 0)
+		if (Interlocked.Decrement(ref _suppressCount) == 0)
 		{
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
 		}
@@ -40,10 +41,7 @@ public class ObservableCollectionEx<T> : ObservableCollection<T>
 		var itemsList = (List<T>)Items;
 		itemsList.InsertRange(index, collection);
 
-		OnCollectionChanged(new NotifyCollectionChangedEventArgs(
-			action: NotifyCollectionChangedAction.Add,
-			changedItem: itemsList.First(),
-			index: index));
+		OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
 	}
 
     protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
